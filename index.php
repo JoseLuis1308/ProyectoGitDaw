fbf3749a01ec0cb158ae8f6e4652867dd20debbc
Ruben de la Orden <ruben_wada_79@hotmail.com>
2019-12-03 10:04:44 +0100

    usuario4->mod index

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-
-/*Archivo modificado */
-
 <html>
 	<head>
 		<title>Proyecto de grupo DAW</title>
 	</head>
 	<body>
 		<h1>Proyecto de DAW</h1>
-
 		<h2>Holiwis</h2>
-		<p>Cambio para branch<p>
+		<p>nuevo</p>
 		<h2>Holiwis.</h2>
 
+		<div>borrado</div>
 	</body>
 </html>
 
