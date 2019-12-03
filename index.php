449dbdc21c21546799b9acf6c42d1bb41dc0c430
JoseLuis1308 <jose.l.l.p.13@gmail.com>
2019-12-03 09:54:09 +0100

    usuario 1

--- index.php
+++ index.php
@@ -1,19 +1,14 @@
-
-/*Archivo modificado */
-
 <html>
 	<head>
 		<title>Proyecto de grupo DAW</title>
 	</head>
 	<body>
 		<h1>Proyecto de DAW</h1>
-<<<<<<< HEAD
 		<h2>Holiwis</h2>
 		<p>nuevo</p>
-=======
 		<h2>Holiwis.</h2>
 
->>>>>>> d6da859089f9da4587fd59725dccf3f4daed354e
+		<div>borrado</div>
 	</body>
 </html>
 
